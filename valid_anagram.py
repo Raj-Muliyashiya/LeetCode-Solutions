b25c313258b7b9c545ab55e1c20de555ab6c384c
RajxMer <rajxmer@gmail.com>
2026-01-08 00:45:44 +0530

    update valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,16 +1,14 @@
-s = "ratt"
-t = "car"
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dic = {}
+        t_dic = {}
 
+        for i in range(len(s)):
+            if len(s) != len(t):
+                return False
 
-dic = {}
+            s_dic[s[i]] = s_dic.get(s[i], 0)+1
+            t_dic[t[i]] = t_dic.get(t[i], 0)+1
 
-for i in range(len(s)):
-    if not dic:
-        if s[i] in dic:
-            dic[s[i]] +=1
-        else:
-            dic[s[i]] = 1
+        return s_dic == t_dic
 
-
-print(dic)
-        
